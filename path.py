2a775b7c0d0f3fbb77bdc23c4b61999a285516ef
Memcys <blue-flame@foxmail.com>
2022-05-31 16:34:16 +0800

    first commit for test of Codespaces

---
+++ path.py
@@ -0,0 +1,27 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: 'Python 3.10.4 (''.venv'': venv)'
+#     language: python
+#     name: python3
+# ---
+
+# This demo imports custom Python package: `package`. Please **install** it first:
+#
+# ```bash
+# pip install -r requirements.txt     # add --user if you are not using virtual environment
+# ```
+
+from package.tree import DisplayablePath
+
+# ### Print directory `package/`
+
+DisplayablePath.print_tree('package/')
+
+
